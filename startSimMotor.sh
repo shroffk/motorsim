2ad019b1a4fc0024d38a4caff212840c2a4f221b
Daron Chabot <dchabot@bnl.gov>
2015-09-17 18:05:24 -0400

    Add (commented) EPICS_CA_ADDR env vars

--- startSimMotor.sh
+++ startSimMotor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export EPICS_CA_AUTO_ADDR_LIST=NO
-export EPICS_CA_ADDR_LIST=127.0.0.1
+#export EPICS_CA_AUTO_ADDR_LIST=NO
+#export EPICS_CA_ADDR_LIST=127.0.0.1
 
 cd ./iocBoot/ioclocalhost && ./st.cmd
 
